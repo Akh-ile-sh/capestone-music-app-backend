333bba1187e351e56bf1a3a7560274341a8bd61a
Akhilesh C <akhileshcvdy@gmail.com>
2024-04-29 17:01:32 +0530

    nodejs and mongodb connected

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+const connectDB = require("./db/connect");
+
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
@@ -9,6 +11,7 @@ app.get("/", (req, res) => {
 });
 
 const start = async () => {
+  await connectDB(process.env.MONGO_URL);
   app.listen(port, () => {
     console.log(`server is listening to port ${port}...`);
   });
