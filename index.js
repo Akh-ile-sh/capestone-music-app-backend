c24113b7d23534fceae7e4bb20d8e2e3741debe8
Akhilesh C <akhileshcvdy@gmail.com>
2024-04-30 10:55:49 +0530

    basic passpoert-jwt setup has been done on index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
 
 const connectDB = require("./db/connect");
 
@@ -9,6 +12,25 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("welcome to  my website");
 });
+
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.JWT_SECRET;
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
+    });
+  })
+);
 
 const start = async () => {
   await connectDB(process.env.MONGO_URL);
